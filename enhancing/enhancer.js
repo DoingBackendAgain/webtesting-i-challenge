1de509f0ab11818dc0b30973d991d531cd6293c8
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-17 18:37:00 -0500

    started

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -20,3 +20,19 @@ function repair(item) {
 function get(item) {
   return { ...item };
 }
+
+
+
+// `./enhancing/enhancer.js` exports an object that has the following methods:
+
+// - a `repair(item)` method that accepts an `item` object and
+//  **returns a new item** with the durability restored to 100. 
+//  This method is the simplest of the three and would be a
+//   **good starting point** on this project.
+// - a `success(item)` method that accepts an `item` object and 
+// **returns a new item** object modified according to the rules
+//  defined by the client for enhancement success.
+// - a `fail(item)` method that accepts an `item` object and
+//  **returns a new item** object modified according to the rules 
+//  defined by the client for enhancement failure.
+// - a `get()` method for use when working on the stretch problem.
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,7 @@
 const enhancer = require('./enhancer.js');
 // test away!
+//npx jest to start
+
+test("runs our first test", () => {
+    expect(1+1).toBe(2)
+})
